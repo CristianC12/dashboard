c55eaaba412d776d6b624b18cdeb54a8e431b8bf
Cristian Martini <cristiamcarlosm@hotmail.com>
2022-01-12 16:47:33 -0300

    Iniciando Dashboard e adicionando primeiro item

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './style.css'
+
+function Banner() {
+    return(
+        <div className='banner-component default-margin default-border'>
+            <div className='textos'>
+                <p className='titulo-banner text-left'>IR agora é no TC</p>
+                <p className='sub-banner text-left'>Clientes Sencon têm acesso aos serviços de IR na plataforma do TC</p>
+            </div>
+        </div>
+    );
+}
+
+export default Banner;
