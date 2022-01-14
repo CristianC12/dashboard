ebbc3c68e3a2da8c5f2b3cec6ae2f96166e24ca4
Cristian Martini <cristiamcarlosm@hotmail.com>
2022-01-14 15:32:38 -0300

    finalizando banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -3,12 +3,15 @@ import './style.css'
 
 function Banner() {
     return (
-        <div className='banner-component default-margin default-border'>
-            <div className='textos'>
-                <p className='titulo-banner text-left'>IR agora é no TC</p>
-                <p className='sub-banner text-left'>Clientes Sencon têm acesso aos serviços de IR na plataforma do TC</p>
+        <>
+            <div className='banner-component default-margin default-border'>
+                <img src='https://i.imgur.com/Op0F128.png' />
+                <div className='textos'>
+                    <p className='titulo-banner text-left'>IR agora é no TC</p>
+                    <p className='sub-banner text-left'>Clientes Sencon têm acesso aos serviços de IR na plataforma do TC</p>
+                </div>
             </div>
-        </div>
+        </>
     );
 }
 
