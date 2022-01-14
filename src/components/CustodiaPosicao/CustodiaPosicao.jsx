840a58a7d31fac8771f3d2487e5741cdda67cf28
Cristian Martini <cristiamcarlosm@hotmail.com>
2022-01-14 14:05:46 -0300

    Completando tabela da posição de custódia

--- src/components/CustodiaPosicao/CustodiaPosicao.jsx
+++ src/components/CustodiaPosicao/CustodiaPosicao.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './style.css';
+import SvgCimaBaixo from './SvgCimaBaixo';
 
 function CustodiaPosicao() {
     return (
@@ -14,6 +15,88 @@ function CustodiaPosicao() {
                 <p className='titulo-card-posicao'>Posição de Custódia</p>
             </div>
 
+            <div>
+                <table className='custodia-table'>
+                    <tr id='tabela-itens'>
+                        <td>
+                            Ativo
+                        </td>
+                        <td>
+                            <SvgCimaBaixo />
+                            Corretora
+                        </td>
+                        <td>
+                            <SvgCimaBaixo />
+                            Quantidade
+                        </td>
+                        <td>
+                            <SvgCimaBaixo />
+                            Custo Médio
+                        </td>
+                        <td>
+                            <SvgCimaBaixo />
+                            Custo Total
+                        </td>
+                    </tr>
+                    <div className='TRcinza'></div>
+                    <tr>
+                        <td>CARE11</td>
+                        <td>XP</td>
+                        <td>000000</td>
+                        <td>R$00,00</td>
+                        <td>R$00.00,000</td>
+                    </tr>
+                    <tr>
+                        <td>CARE11</td>
+                        <td>XP</td>
+                        <td>000000</td>
+                        <td>R$00,00</td>
+                        <td>R$00.00,000</td>
+                    </tr>
+                    <div className='TRcinza'></div>
+                    <tr>
+                        <td>CARE11</td>
+                        <td>XP</td>
+                        <td>000000</td>
+                        <td>R$00,00</td>
+                        <td>R$00.00,000</td>
+                    </tr>
+                    <tr>
+                        <td>CARE11</td>
+                        <td>XP</td>
+                        <td>000000</td>
+                        <td>R$00,00</td>
+                        <td>R$00.00,000</td>
+                    </tr>
+                    <div className='TRcinza'></div>
+                    <tr>
+                        <td>CARE11</td>
+                        <td>XP</td>
+                        <td>000000</td>
+                        <td>R$00,00</td>
+                        <td>R$00.00,000</td>
+                    </tr>
+                </table>
+            </div>
+            <div className='table-pages'>
+                <button>
+                    <svg width="7" height="10" viewBox="0 0 7 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" clip-rule="evenodd" d="M6.08647 0.542893C6.477 0.933418 6.477 1.56658 6.08647 1.95711L3.04358 5L6.08647 8.04289C6.47699 8.43342 6.47699 9.06658 6.08647 9.45711C5.69595 9.84763 5.06278 9.84763 4.67226 9.45711L0.922256 5.70711C0.73472 5.51957 0.629363 5.26522 0.629363 5C0.629363 4.73478 0.73472 4.48043 0.922257 4.29289L4.67226 0.542893C5.06278 0.152369 5.69595 0.152369 6.08647 0.542893Z" fill="#4364E8" />
+                    </svg>
+                </button>
+
+                <div>
+                    <p>
+                        1
+                    </p>
+                </div>
+
+                <button>
+                    <svg width="7" height="10" viewBox="0 0 7 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" clip-rule="evenodd" d="M0.922257 9.45711C0.531733 9.06658 0.531733 8.43342 0.922257 8.04289L3.96515 5L0.922257 1.95711C0.531732 1.56658 0.531733 0.933416 0.922257 0.542892C1.31278 0.152369 1.94595 0.152368 2.33647 0.542892L6.08647 4.29289C6.47699 4.68342 6.47699 5.31658 6.08647 5.70711L2.33647 9.45711C1.94595 9.84763 1.31278 9.84763 0.922257 9.45711Z" fill="#4364E8" />
+                    </svg>
+                </button>
+            </div>
 
         </div>
     );
