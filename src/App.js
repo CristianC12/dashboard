c0acea0247668eb782d6d56a0f23c04b647d3a5a
Cristian Martini <cristiamcarlosm@hotmail.com>
2022-01-18 18:19:42 -0300

    ajustes

--- src/App.js
+++ src/App.js
@@ -1,30 +1,32 @@
-import './App.css';
-import Banner from './components/Banner/Banner';
-import ImpostoRenda from './components/ImpostoRenda/ImpostoRenda';
-import Relatorio from './components/Relatorio/Relatorio';
-import CustodiaDistribuicao from './components/CustodiaDistribuicao/CustodiaDistribuicao';
-import CustodiaPosicao from './components/CustodiaPosicao/CustodiaPosicao';
-import ControleResultados from './components/ControleResultados/ControleResultados';
-import MenuSuperior from './components/MenuSuperior/MenuSuperior';
+import "./App.css";
+import Banner from "./components/Banner/Banner";
+import ImpostoRenda from "./components/ImpostoRenda/ImpostoRenda";
+import Relatorio from "./components/Relatorio/Relatorio";
+import CustodiaDistribuicao from "./components/CustodiaDistribuicao/CustodiaDistribuicao";
+import CustodiaPosicao from "./components/CustodiaPosicao/CustodiaPosicao";
+import ControleResultados from "./components/ControleResultados/ControleResultados";
+import MenuSuperior from "./components/MenuSuperior/MenuSuperior";
 
 function App() {
   return (
     <div className="App">
       <MenuSuperior />
       <Banner />
-      <div className='components default-margin-components'>
+      <div className="components default-margin-components">
         <CustodiaDistribuicao />
         <ImpostoRenda />
         <Relatorio />
       </div>
-      <div className='components default-margin-components'>
+      <div className="components default-margin-components">
         <CustodiaPosicao />
         <ControleResultados />
       </div>
-      
-      <label id='TESTE2' for="TESTE"> Trocar tema da pagina *TERMINAR*</label>
-      <input id='TESTE' name="theme" type="checkbox" class="toggle-dark-mode" />
 
+      <label id="TESTE2" for="TESTE">
+        {" "}
+        Trocar tema da pagina *TERMINAR*
+      </label>
+      <input id="TESTE" name="theme" type="checkbox" class="toggle-dark-mode" />
     </div>
   );
 }
